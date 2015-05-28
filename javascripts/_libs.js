0d0b6289b37d3e7cb9270bd82b4a458391fec7e7
Zhengyu Qian <laerhsif@gmail.com>
2015-05-28 19:55:16 +0800

    Add JS dependencies.

---
+++ javascripts/_libs.js
@@ -0,0 +1,6 @@
+/* @preserve
+ * jQuery v2.1.4 | (c) 2005, 2015 jQuery Foundation, Inc. | jquery.org/license
+ * jQuery Cookie Plugin v1.4.1 | (c) 2013 Klaus Hartl | MIT License |github.com/carhartl/jquery-cookie
+ * uuid.js | (c) 2010-2012 Robert Kieffer | MIT License
+ */
+ 
