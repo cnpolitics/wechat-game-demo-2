182c2b0947fc29b0e9091de118bc4fbe03929a09
Zhengyu Qian <laerhsif@gmail.com>
2015-05-29 19:38:01 +0800

    Implement the transition between copies. + Update the HTML structure of the copies. + Refine the effect of the transition between hero images.

--- javascript/page-transition.js
+++ javascript/page-transition.js
@@ -1,16 +1,24 @@
 (function() {
 	'use strict';
-	var currQuestIdx = 0;
+	
+	var currQuestIdx = 0,
+	    nextQuestIdx = 1;
+		
 	$('.opt-btn').click(function() {
-		$('.quest-' + currQuestIdx).removeClass('active');
+		// Hero image transition
+		$('.hero-image .quest-' + currQuestIdx).removeClass('active');
+		$('.hero-image .quest-' + nextQuestIdx).addClass('active');
 		
-		currQuestIdx++;
-		if (currQuestIdx > 7) { // for debug
-			currQuestIdx = 0;
+		// Question copy transition
+		$('.question').removeClass('active quest-' + currQuestIdx).addClass('quest-' + nextQuestIdx);
+		setTimeout(function() {
+			$('.question').addClass('active');
+		}, 750);
+		
+		currQuestIdx = nextQuestIdx;
+		nextQuestIdx++;
+		if (nextQuestIdx > 7) {
+			nextQuestIdx = 0;	// Loop for debug
 		}
-		
-		$('.quest-' + currQuestIdx).addClass('active');
-		
-		
 	});
 })();
