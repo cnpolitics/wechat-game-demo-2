070150e7c17fe3399fdb06aeb4fee917bc9f2918
Zhengyu Qian <laerhsif@gmail.com>
2015-05-30 03:41:45 +0800

    Update the scripts for controlling the transition effects. + Rename the JS file. + Add the transition for the hero-image of cover. + Add the transition for the start button in cover. + Add the transition for the option buttons' first loading.

---
+++ javascript/transition.js
@@ -0,0 +1,47 @@
+/* global $ */
+
+(function() {
+	'use strict';
+	
+	var currQuestIdx = 1,
+	    nextQuestIdx = 2;
+	
+	$('.start-btn').click(function() {
+		// Hero image transtion.
+		$('.hero-image .cover').removeClass('active');
+		$('.hero-image .quest-1').addClass('active');
+		
+		// Button transition.
+		$(this).removeClass('active');
+		setTimeout(function() {
+			$('.start-btn').hide();
+			$('.opt-btn').show().addClass('active delay');
+		}, 500);
+		
+		setTimeout(function() {
+			$('.opt-btn').removeClass('delay');
+		}, 1100);
+		
+		
+	});
+		
+	$('.opt-btn').click(function() {
+		// Hero image transition.
+		$('.hero-image .quest-' + currQuestIdx).removeClass('active');
+		$('.hero-image .quest-' + nextQuestIdx).addClass('active');
+		
+		// Question copy transition.
+		$('.copy')
+			.removeClass('active quest-' + currQuestIdx)
+			.addClass('quest-' + nextQuestIdx);
+		setTimeout(function() {
+			$('.copy').addClass('active');
+		}, 750);
+		
+		currQuestIdx = nextQuestIdx;
+		nextQuestIdx++;
+		if (nextQuestIdx > 8) {
+			nextQuestIdx = 1;	// Loop for debug
+		}
+	});
+})();
