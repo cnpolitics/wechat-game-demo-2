a02bbb1bb09aa667e0dc5590b066eabc1eb53eb5
Zhengyu Qian <laerhsif@gmail.com>
2015-06-03 21:23:41 +0800

    Make URL controls. + Design hash URL scheme (#result=x&id=xxxxxxxx-xxxx-4xxx-xxxx-xxxxxxxxxxxx). + Write result into cookie, hash URL and doc title at the result page. + Parse URL, and construct the forks to set the cover page.

--- javascript/exam.js
+++ javascript/exam.js
@@ -1,5 +1,5 @@
-/* global $, Shojo, uuid*/
-(function() {
+/* global $, Shojo, uuid, shojo*/
+(function(shojo) {
 	'use strict';
 	
 	var SCORE_CHART = [
@@ -13,14 +13,13 @@
 		[3, 4, 2, 0, 5]
 	];
 	
-	var shojo = new Shojo(),
-		currQuestIdx = 1;
+	// Get value by key from hash URL.
+	function getValueByKey(str, key) {
+		var matches = str.match(new RegExp(key+'=([^&]*)'));
+		return matches ? matches[1] : null;
+	}
 	
-	// Set user ID, and write cookie.
-	shojo.id = $.cookie('mzNeZha.id') || uuid.v4();
-	$.cookie('mzNeZha.id', shojo.id);
-	
-	// For debug.
+	// Map the score to the result.
 	function calcResult(score) {
 		if (score <= 16) {
 			shojo.result = 1;
@@ -34,27 +33,81 @@
 		return shojo.result;
 	}
 	
+	// Set user ID, and write cookie.
+	shojo.id = $.cookie('mzNeZha.id') || uuid.v4();
+	$.cookie('mzNeZha.id', shojo.id);
+	
+	// Parse URL when visit the webpage.
+	$(document).ready(function() {
+		var hashURL = window.location.hash;
+		var result = getValueByKey(hashURL, 'result'),
+			userid = getValueByKey(hashURL, 'id');
+		console.log(hashURL);
+		console.log(result);
+		console.log(userid);
+		
+		if (result === null) {
+			console.log('No result.');
+			return;
+		} else if (userid === shojo.id) {
+			console.log('This is myself.');
+			// View my last result page.
+			
+		} else {
+			// View the custom cover from URL.
+			switch (result) {
+				case '1':
+					break;
+				case '2':
+					break;
+				case '3':
+					break;
+				case '4':
+					break;
+				default:
+
+			}
+		}
+	});
+	
+	
+	
+	//====== DO THE EXAM.
+	var currQuestIdx = 1;
+	
 	$('.opt-btn').click(function() {
 		// Safeguard.
-		if (currQuestIdx > 8) {
-			alert('Result: ' + calcResult(shojo.getScore(SCORE_CHART))); // For debug
-			return;
-		}
+		if (currQuestIdx > 8) { return; }
 		
-		//console.log($(this).data('opt'));
 		shojo.select(currQuestIdx, parseInt($(this).data('opt'), 10));
-		//console.log(shojo.getScore(SCORE_CHART));
 		
-		// Calc result, and write cookie.
+		// Calc result; write cookie, hash URL and doc title.
 		if (currQuestIdx === 8) {
+			// Calc result, and write cookie.
 			$.cookie('mzNeZha.result', calcResult(shojo.getScore(SCORE_CHART)));
 			
-			//alert('Result: ' + $.cookie('mzNeZha.result')); // For test
+			// Write hash URL.
+			window.location.hash = 'result=' + shojo.result + '&id=' + shojo.id;
+			
+			// Write doc title.
+			switch (shojo.result) {
+				case 1:
+					document.title = '没救了！';
+					break;
+				case 2:
+					document.title = '离少女越来越远！';
+					break;
+				case 3:
+					document.title = '有少女的一面！';
+					break;
+				case 4:
+					document.title = '少女指数爆棚！';
+					break;
+				default: 
+
+			}
 		}
 		
 		currQuestIdx++;
 	});
-	
-	window.shojo = shojo;
-	return window.shojo;
-})();
+})(shojo);
