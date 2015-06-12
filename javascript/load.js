63561f5abab0a28578f8c9199d0dd84cab4e4cc8
Zhengyu Qian <laerhsif@gmail.com>
2015-06-13 02:43:17 +0800

    Add a simple script for loading screen logic.

---
+++ javascript/load.js
@@ -0,0 +1,12 @@
+(function() {
+	'use strict';
+
+	$(window).load(function() {
+		console.log('test');
+		$('.load-screen').addClass('active');
+		setTimeout(function() {
+			$('.load-screen').hide();
+		}, 2500);
+	
+	});
+}());
