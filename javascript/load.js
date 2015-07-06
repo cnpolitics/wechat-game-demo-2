0382d94d16394e87da9df5677febf2446fe8dd46
Zhengyu Qian <laerhsif@gmail.com>
2015-07-06 20:42:45 +0800

    Add controlling script for the preloading.

--- javascript/load.js
+++ javascript/load.js
@@ -1,12 +1,13 @@
+/* global $ */
+
 (function() {
 	'use strict';
 
 	$(window).load(function() {
 		// console.log('test');
-		$('.load-screen').addClass('active');
+		$('.load-screen, .load-widget').addClass('active');
 		setTimeout(function() {
 			$('.load-screen').hide();
-		}, 2500);
-	
+		}, 1500);
 	});
 }());
